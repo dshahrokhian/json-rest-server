5c0f3d2f6ea9c64b625585087b550e3949a76877
Daniyal <dani.shahrokhian@gmail.com>
2016-11-28 05:59:13 +0100

    Add static behavior.

--- src/sneakPeak/JSON2InterestEvent.scala
+++ src/sneakPeak/JSON2InterestEvent.scala
@@ -3,14 +3,14 @@ package sneakPeak
 import org.json4s._
 import org.json4s.native.JsonMethods._
 
-class JSON2InterestEvent {
+object JSON2InterestEvent {
   implicit val formats = DefaultFormats
   
-  final def string2interestEvent(json: String) : InterestEvent = {
+  def string2interestEvent(json: String) : InterestEvent = {
     return parse(json).extract[InterestEvent]
   }
   
-  final def file2interestEvent(fileName: String) : InterestEvent = {
+  def file2interestEvent(fileName: String) : InterestEvent = {
     var json: String = ""
     
     for (line <- io.Source.fromFile(fileName).getLines) json += line
