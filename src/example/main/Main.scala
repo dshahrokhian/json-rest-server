4969ed8b2d2d0cdb9a9cb8baf42ff20de0c422e6
Daniyal <dani.shahrokhian@gmail.com>
2016-11-28 18:26:35 +0100

    Restructure project.

--- src/example/main/Main.scala
+++ src/example/main/Main.scala
@@ -1,29 +1,36 @@
 package example.main
 
-import sneakPeek.SneakPeekAlgorithm
-import java.awt.image.BufferedImage
-import javax.imageio.ImageIO
 import java.io.File
+
+import example.validation.MarkedAreasVisualization
+import javax.imageio.ImageIO
 import sneakPeek.JSON2InterestEvent
-import sneakPeek.JSON2InterestEvent
+import sneakPeek.SneakPeekAlgorithm
+import example.validation.JSON2MarkedAreas
 
 object Main {
   def main(args: Array[String]) {
     
     val imagePath = "src/example/resources/";
-    val dataPath = "src/example/resources/userData/"
+    val dataPath = imagePath + "userData/test/"
+    val validationPath = imagePath + "userData/validation/"
     
-    val image = ImageIO.read(new File(imagePath + "image.jpg"))
+    val image = ImageIO.read(new File(imagePath + "test_image.jpg"))
     val algo = new SneakPeekAlgorithm(image)
+    val validation = new MarkedAreasVisualization(image)
     
     for (i <- 0 to 18) {
       val ev = JSON2InterestEvent.file2interestEvent(dataPath + i + ".json")
       algo.addEvent(ev)
     }
-    
     val heatMap = algo.getHeatMapOverlay()
     
+    val markedAreas = JSON2MarkedAreas.file2markedAreas(validationPath + "val.json")
+    validation.setAreas(markedAreas)
+    val markedMap = validation.getMarkedAreasOverlay()
+    
     // Save as new image
-    ImageIO.write(heatMap, "PNG", new File(imagePath, "result.png"));
+    ImageIO.write(heatMap, "PNG", new File(imagePath, "result_image.png"));
+    ImageIO.write(markedMap, "PNG", new File(imagePath, "marked_image.png"));
   }
 }
