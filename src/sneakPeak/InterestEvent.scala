92b5bdeb4e482d7d614e11db3cc27cc74fa6ddf1
Daniyal <dani.shahrokhian@gmail.com>
2016-11-28 05:58:20 +0100

    Fix fields to be equal to the ones from the JSON file.

--- src/sneakPeak/InterestEvent.scala
+++ src/sneakPeak/InterestEvent.scala
@@ -1,9 +1,9 @@
 package sneakPeak
 
-case class InterestEvent(upperLeftX: Int,
-    upperLeftY: Int,
-    bottomRightX: Int,
-    bottomRightY: Int,
-    timestamp: Long,
-    isFinal: Boolean,
-    opOrder: Int)
+case class InterestEvent(upper_left_x: Int,
+    upper_left_y: Int,
+    bottom_right_x: Int,
+    bottom_right_y: Int,
+    time: Long,
+    is_final: Boolean,
+    op_order: Int)
